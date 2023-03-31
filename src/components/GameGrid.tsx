722063a90bc172ad20534bf7e856e7d04647570b
Gaya Haroun <gharoun94@gmail.com>
2023-03-31 11:59:45 -0500

    fiter games by genre

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,11 +1,15 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import { Genre } from "../hooks/useGenres";
 import GameCard from "./GameCard";
 import GameCardContainer from "./GameCardContainer";
 import GameCardSketeton from "./GameCardSketeton";
 
-const GameGrid = () => {
-  const { data, error, isLoading } = useGames();
+interface Props {
+  SelectedGenre: Genre | null;
+}
+const GameGrid = ({ SelectedGenre }: Props) => {
+  const { data, error, isLoading } = useGames(SelectedGenre);
   const Skeletons = [1, 2, 3, 4, 5, 6];
   return (
     <>
@@ -17,13 +21,13 @@ const GameGrid = () => {
       >
         {isLoading &&
           Skeletons.map((skeleton) => (
-            <GameCardContainer>
-              <GameCardSketeton key={skeleton} />
+            <GameCardContainer key={skeleton}>
+              <GameCardSketeton />
             </GameCardContainer>
           ))}
         {data.map((game) => (
-          <GameCardContainer>
-            <GameCard key={game.id} game={game} />
+          <GameCardContainer key={game.id}>
+            <GameCard game={game} />
           </GameCardContainer>
         ))}
       </SimpleGrid>
